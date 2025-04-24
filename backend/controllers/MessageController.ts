d905472f9d833f9ed0c8d86aff8b4c4cb354ba3a
kenpwm <142961950+Ken-pwm@users.noreply.github.com>
2025-04-24 16:03:55 +0800

    Update MessageController.ts

--- backend/controllers/MessageController.ts
+++ backend/controllers/MessageController.ts
@@ -191,10 +191,6 @@ class MessageController {
             const promptResponse = await axios.post(
               "http://localhost:11434/api/chat",
               {
-               // model: "hf.co/Eve-31415/fitness-training",
-                
-               
-               
                 model: "hf.co/Eve-31415/fitness-training",
                 messages: history,
                 stream: false,
